8a8996b3dd7a75b5735be57bb6aab445b9d1b09b
VectorSoftworks <appam_000@XSoloDOlo>
2014-06-05 20:43:36 -0700

    Added some more functionality n shit not really much

--- src/net/vectorgaming/mediarealm/ui/controllers/MyVideoHandler.java
+++ src/net/vectorgaming/mediarealm/ui/controllers/MyVideoHandler.java
@@ -10,6 +10,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import org.apache.commons.io.FilenameUtils;
+
 
 
 /*The event handler for the myVideo Button*/
@@ -17,6 +19,66 @@ public class MyVideoHandler implements EventHandler {
 
     private ArrayList<String> directories;
     public static boolean needScan = true;
+    private static final String[] EXTENSIONS_VIDEO = {
+        "3g2",
+        "3gp",
+        "3gp2",
+        "3gpp",
+        "amv",
+        "asf",
+        "avi",
+        "bin",
+        "divx",
+        "drc",
+        "dv",
+        "f4v",
+        "flv",
+        "gvi",
+        "gxf",
+        "iso",
+        "m1v",
+        "m2v",
+        "m2t",
+        "m2ts",
+        "m4v",
+        "mkv",
+        "mov",
+        "mp2",
+        "mp2v",
+        "mp4",
+        "mp4v",
+        "mpe",
+        "mpeg",
+        "mpeg1",
+        "mpeg2",
+        "mpeg4",
+        "mpg",
+        "mpv2",
+        "mts",
+        "mtv",
+        "mxf",
+        "mxg",
+        "nsv",
+        "nuv",
+        "ogg",
+        "ogm",
+        "ogv",
+        "ogx",
+        "ps",
+        "rec",
+        "rm",
+        "rmvb",
+        "tod",
+        "ts",
+        "tts",
+        "vob",
+        "vro",
+        "webm",
+        "wm",
+        "wmv",
+        "wtv",
+        "xesc"
+    };
     //Creat class and package location for the VideoButtons Class
     //public static ArrayList <VideoButtons> videoButtonsList = new ArrayList<VideoButtons>();
 
@@ -28,9 +90,10 @@ public class MyVideoHandler implements EventHandler {
             getDirectories();
             loadFiles();
         }
-
         addButtons();
     }
+   
+    
     /*Scans the file "Directories" loacted in net\vectorgaming\mediarealm\config
      and for each directory found creates a string with the directory and loads 
      it into the directories Array
@@ -42,8 +105,9 @@ public class MyVideoHandler implements EventHandler {
         String line;
         BufferedReader reader = null;
         File directoriesConfig;
-        try {
-            directoriesConfig = new File("C:/Users/XSoloDoloX/Documents/GitHub/MediaRealmOfficial/src/net/vectorgaming/mediarealm/config/Directories.txt");
+        try 
+        {
+            directoriesConfig = new File("C:/Users/appam_000/Documents/GitHub/MediaRealmOfficial/src/net/vectorgaming/mediarealm/config/Directories.txt");
             reader = new BufferedReader(new FileReader(directoriesConfig));
 
         } 
@@ -51,34 +115,49 @@ public class MyVideoHandler implements EventHandler {
         {
             Logger.getLogger(MyVideoHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        try 
-        {
-            while (reader.ready()) 
-            {
+        try {
+            while (reader.ready()) {
                 line = reader.readLine();
                 System.out.println(line);
                 directories.add(line);
             }
-        } 
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
 
         }
 
     }
-    /*Scans the directories given in the @directorie array  for video files
+   
+    /*Scans the directories given in the @directory array  for video files
      and with each file found creates an object 
      and loads said object into an arrayList @VideoPointers*/
 
     private void loadFiles() {
-        for (String directory : directories) {
+        for (String directory : directories) 
+        {
+            File dir = new File(directory);
+            
+            File[] files = dir.listFiles();
+           
+            if (files != null){
+            for(File f : files)
+           {
+               for(String name : EXTENSIONS_VIDEO)
+               
+                   if (FilenameUtils.getExtension(f.getName()).equalsIgnoreCase(name))
+               {
+                   System.out.println(f.getAbsolutePath());
+               }
+           }
+           
+            }
 
         }
     }
 
     /*adds all the buttons in the Arraylist @videoButtonsList to  
      the flow pane located in the content pane*/
-    private void addButtons() {
+    private void addButtons() 
+    {
 
     }
 }
